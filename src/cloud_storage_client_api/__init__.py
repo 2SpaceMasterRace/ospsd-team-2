e03f7dd9ebd46ff836f37505d64eb04d705ab533
Aarav Agrawal <aarav.goldi@gmail.com>
2026-02-28 19:30:19 -0500

    worked on DI

--- src/cloud_storage_client_api/__init__.py
+++ src/cloud_storage_client_api/__init__.py
@@ -1,3 +1,3 @@
-from .client import CloudStorageClient
+from cloud_storage_client_api.client import CloudStorageClient, get_client
 
-__all__ = ["CloudStorageClient"]
+__all__ = ["CloudStorageClient", "get_client"]
